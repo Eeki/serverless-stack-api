cb9485fa901b080732f5aa853492c299b20e5ef7
eliel <eliel.niukkanen@medisapiens.fi>
2020-07-27 21:21:34 +0300

    Adding our Serverless API

---
+++ libs/billing-lib.js
@@ -0,0 +1,10 @@
+const getRate = (storage) => {
+  if (storage <= 10) { return 4; }
+  if (storage <= 100) { return 2; }
+  return 1;
+};
+
+export function calculateCost(storage) {
+  const rate = getRate(storage);
+  return rate * storage * 100;
+}
